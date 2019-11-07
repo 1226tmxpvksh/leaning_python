8627ba5efca8e1f62867664093a23a6eb1aee7e6
1226tmxpvksh <1226tmxpvksh@naver.com>
2019-11-07 21:05:58 +0900

    파이썬 자료형 연습

---
+++ py_1107/py_1107/py_1107.py
@@ -0,0 +1,65 @@
+print(1+1)
+print(52)
+print(273)
+print(1,'\n',2)
+print(1,'\n','qeqeqeqeqe',22345678,sep="                 ")
+print('Hello World')
+
+a = 2
+print(a)
+
+a = "asdasdasdad"
+print(a)
+
+a = 1.123123
+print(a)
+
+a = 'asd'
+print(a)
+
+a=input()
+print(type(a))
+print(a+'1234')
+
+a=int(input())
+print(a)
+print(a+1234)
+
+print('"안녕하세요"라고 말했습니다')
+print("'배가 고픕니다'라고 생각했습니다")
+print("\"안녕하세요\"라고 말했습니다")
+print('\'배가 고픕니다\'라고 생각했습니다')
+
+print("asdf"*3)
+
+print("문자 선택 연산자에 대해 알아볼까요?")
+print("안녕하세요"[0])
+print("안녕하세요"[1])
+print("안녕하세요"[2])
+print("안녕하세요"[3])
+print("안녕하세요"[4])
+
+print("안녕하세요"[1:4])
+
+alp='abcdefghijklmnopqrstuvwxyz'
+print(alp[5:11])
+print(alp[18:26])
+print(alp[-26:-18])
+print(len(alp))
+for i in alp:
+	print(i)
+
+print(len("안녕하세요"))
+
+a=15
+b=8
+print(a+b,a-b,a*b,a/b,a%b,a//b,a**b)
+
+a=input()
+print(a)
+
+tmp=''
+for i in alp:
+	tmp +=i
+	print(tmp)
+
